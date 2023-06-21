5a818d16f94560cc1f1dc8ac8e5cc7c0e815de0e
ilay-sn <ilayda.snck.02@gmail.com>
2023-06-21 14:31:28 +0300

    pages are linked, scroll funtions are added

---
+++ app/src/main/java/com/mensaapplication/MainActivity2.java
@@ -0,0 +1,83 @@
+package com.mensaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        imageView = findViewById(R.id.imageView31);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent class will help to go to next activity using
+                // it's object named intent.
+                // SecondActivty is the name of new created EmptyActivity.
+                //Intent intent = new Intent(MainActivity.this, layout.class);
+                //startActivity(intent);
+                setContentView(R.layout.layout2);
+            }
+        });
+        imageView2 = findViewById(R.id.imageView32);
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent class will help to go to next activity using
+                // it's object named intent.
+                // SecondActivty is the name of new created EmptyActivity.
+                //Intent intent = new Intent(MainActivity.this, layout.class);
+                //startActivity(intent);
+                setContentView(R.layout.layout2);
+            }
+        });
+        imageView3 = findViewById(R.id.imageView30);
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent class will help to go to next activity using
+                // it's object named intent.
+                // SecondActivty is the name of new created EmptyActivity.
+                //Intent intent = new Intent(MainActivity.this, layout.class);
+                //startActivity(intent);
+                setContentView(R.layout.layout2);
+            }
+        });
+        imageView4 = findViewById(R.id.imageView5);
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent class will help to go to next activity using
+                // it's object named intent.
+                // SecondActivty is the name of new created EmptyActivity.
+                //Intent intent = new Intent(MainActivity.this, layout.class);
+                //startActivity(intent);
+                setContentView(R.layout.layout2);
+            }
+        });
+        imageView5 = findViewById(R.id.imageView26);
+        imageView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent class will help to go to next activity using
+                // it's object named intent.
+                // SecondActivty is the name of new created EmptyActivity.
+                //Intent intent = new Intent(MainActivity.this, layout.class);
+                //startActivity(intent);
+                setContentView(R.layout.layout2);
+            }
+        });
+    }
+}
