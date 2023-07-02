a384c944f1896d8b441bc8da0d186844e362d033
ilay-sn <ilayda.snck.02@gmail.com>
2023-07-03 01:24:30 +0300

    meals and reviews pages are linked, meals recyclerview is done

--- app/src/main/java/com/mensaapplication/MainActivity.java
+++ app/src/main/java/com/mensaapplication/MainActivity.java
@@ -1,11 +1,17 @@
 package com.mensaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView14);
+        int cafetaria1 = imageView.getId();
+        Log.d("ilayda", String.valueOf(cafetaria1));
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,18 +34,26 @@ public class MainActivity extends AppCompatActivity {
                 // it's object named intent.
                 // SecondActivty is the name of new created EmptyActivity.
                 //setContentView(R.layout.layout);
+                //List<meals> meals = new ArrayList<meals>();
+                //meals.add(new meals("Angebot 1","Gemüseschnitzel mit Soja- Kräutersauce Rustika Carrots und Petersilienkartoffeln", "2,50€","4,50€",R.drawable.geflugel,R.drawable.star_empty));
+                /*String[] meals = new String[3];
+                meals[0]="Value1";
+                meals[1]="Value2";
+                meals[2]="Value3";*/
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("cafetaria_id",String.valueOf(cafetaria1));
                 startActivity(intent);
             }
         });
         imageView2 = findViewById(R.id.imageView16);
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 // Intent class will help to go to next activity using
                 // it's object named intent.
                 // SecondActivty is the name of new created EmptyActivity.
                 //setContentView(R.layout.layout);
+
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
             }
